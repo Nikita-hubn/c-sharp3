57a13bc52f38bcc6e8643a56fb803be11b606da5
Nikita-hubn <73433620+Nikita-hubn@users.noreply.github.com>
2023-04-29 13:50:36 +0300

    Add files via upload

---
+++ Auto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Auto
+    {
+        private int _Speed;
+        private int _Fuel;
+        private int _Price;
+        private string _Name;
+
+        public Auto(int speed, int fuel, string name, int price) //конструктор
+        {
+            _Name = name;
+            _Speed = speed;
+            _Fuel = fuel;
+            _Price = price;
+        }
+        public string GetName()
+        {
+            return _Name;
+        }
+
+        public void SetName(string name)
+        {
+            _Name = name;
+        }
+        public int GetSpeed()
+        {
+            return _Speed;
+        }
+
+        public void SetName(int speed)
+        {
+            _Speed = speed;
+        }
+
+        public int GetPrice()
+        {
+            return _Price;
+        }
+
+        public void SetPrice(int price)
+        {
+            _Price = price;
+        }
+        public int GetFuel()
+        {
+            return _Fuel;
+        }
+        public void SetFuel(int fuel)
+        {
+            _Fuel = fuel;
+        }
+        public string MakeStr() // возвращает строку для вывода в листбокс
+        {
+            return "Название: " + GetName() + "цена: " + GetPrice() + ", Расход топлива: " + GetFuel() + ", Скорость: " + GetSpeed();
+        }
+    }
+}
---
+++ taxopark.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class taxopark
+    {
+        public List<Auto> taxi;
+        public taxopark()
+        {
+            taxi = new List<Auto>();
+            taxi.Add(new Auto(220, 65, "reno", 400000));
+            taxi.Add(new Auto(200, 85, "lada", 220000));
+            taxi.Add(new Auto(300, 70, "bmw", 1220000));
+            taxi.Add(new Auto(270, 60, "kia", 720000));
+        }
+        public int Price() 
+        {
+            int sum = 0;
+            for (int i = 0; i < taxi.Count(); i++)
+            {
+                sum += taxi[i].GetPrice();
+            }
+            return sum;
+        }
+        public string FindByRange(int first, int last) 
+        {
+            string str = "";
+            int len;
+            for (int i = 0; i < taxi.Count(); i++)
+            {
+                len = taxi[i].GetSpeed();
+                if (len >= first && len <= last)
+                {
+                    str += taxi[i].MakeStr() + " ";
+                }
+            }
+            return str;
+        }
+    }
+}
